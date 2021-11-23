56affe0ad2af6c838e7716009759ff5f0ab3eab2
Rudinei Costa <costaaugusto177@outlook.com>
2021-11-22 23:19:47 -0300

    final

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`🚀 Server started on port ${process.env.PORT || 3333}!`);
 });
